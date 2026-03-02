a02c5e25928679741974fbf4bf89901d8eb085c1
claymicholaz <clay.micholaz@profescipta.co.id>
2026-03-02 08:30:07 +0700

    feat: login page adjustment

--- auth/login.php
+++ auth/login.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HR Information System</title>
     <link rel="stylesheet" href="../assets/style_login.css">
 </head>
+
 <body>
     <div class="login-card">
         <div>
@@ -15,11 +17,13 @@
         <form action="proses_login.php" method="post">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
-             <button type="submit" name="login">Sign In</button>
+            <button type="submit" name="login">Sign In</button>
+            <button type="button" id="register" name="register" onclick="window.location.href='register.php'">Register</button>
         </form>
         <div class="login-footer">
             &copy <?= date('Y'); ?> Human Resource Information System
         </div>
     </div>
 </body>
+
 </html>
