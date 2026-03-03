8ddb24b9887aec96f0431da6eaaaec0c0ce22d89
EgnerCons <egnercons@gmail.com>
2026-03-03 19:09:02 +0700

    egner

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -5,7 +5,7 @@ include "layout/header.php";
 include "layout/sidebar.php";
 ?>
 <main>
-    <h2>Dashboard Admin</h2>
+    <h2>Dashboard HRD</h2>
     <p>Kelola Data Pegawai dan Jabatan</p>
 </main>
 </div>
